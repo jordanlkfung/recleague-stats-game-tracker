4d859e6a0b76985e49c50dc57724cfee062ac01c
jordanlkfung <jordanlkfung@gmail.com>
2024-11-30 14:05:12 -0800

    signup changes

--- frontend/app/api/signup/route.ts
+++ frontend/app/api/signup/route.ts
@@ -1,3 +1,4 @@
+import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 // Helper function to handle errors
@@ -7,5 +8,27 @@ const handleError = (message: string, status: number) => {
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
+    const { email, password } = body;
 
+    if (!email || !password) {
+        return handleError('Email and password are required', 400);
+    }
+
+    try {
+        const response = await fetch(`${process.env.SERVER_HOST}/user`,
+            {
+                method: 'POST',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify({ email, password })
+            })
+        if (!response.ok) {
+            const errorData = await response.json();
+            return NextResponse.json({ error: errorData }, { status: response.status });
+        }
+        const data = await response.json();
+        return NextResponse.json(data, { status: 201 });
+    }
+    catch (e) {
+        return handleError("Failed to create new user", 500);
+    }
 }
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -3,26 +3,42 @@ import { useState } from 'react';
 import { useRouter } from "next/navigation";
 
 export default function signup() {
-
+    const router = useRouter();
     const [email, setEmail] = useState("");
     const [emailError, setEmailError] = useState<String>('');
     const [password, setPassword] = useState("");
     const [confirmpassword, setConfirmPassword] = useState('')
     const [passwordsMatch, setPasswordsMatch] = useState(true);
 
-    const handleFormSumbit = () => {
+    const handleFormSumbit = async (event: React.FormEvent<HTMLButtonElement>) => {
+        event.preventDefault();
         if (passwordsMatch && validEmail()) {
+            try {
+                const response = await fetch('/api/signup', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ email, password })
+                });
+
+                if (response) {
+                    router.push("")
+                }
+            } catch (e) {
+                //ERROR SHOW SNACKBAR
+            }
 
         }
     };
 
     const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(event.target.value);
-        setPasswordsMatch(event.target.value === confirmpassword)
+        setPasswordsMatch(event.target.value === confirmpassword);
+        disableSignUpButton();
     }
     const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setConfirmPassword(event.target.value);
-        setPasswordsMatch(password === event.target.value)
+        setPasswordsMatch(password === event.target.value);
+        disableSignUpButton()
     }
     const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(event.target.value);
@@ -30,10 +46,18 @@ export default function signup() {
             setEmailError("");
         else
             setEmailError('Invalid Email');
+        disableSignUpButton();
     }
     const validEmail = () => {
         let match = email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
         return !(match === null);
+    }
+    const disableSignUpButton = () => {
+        const button = document.getElementById("signUpBtn")!;
+        if (emailError || !passwordsMatch)
+            button.setAttribute('disabled', '');
+        else
+            button.removeAttribute('disabled');
     }
 
     return (
@@ -47,7 +71,7 @@ export default function signup() {
                 <label htmlFor="comfirm password" className='mb-0'>Confirm Password</label>
                 <input type="password" className="rounded-md text-black p-3 w-80" name="confirmpassword" id="password2" onChange={handleConfirmPasswordChange} value={confirmpassword} required></input>
                 {!passwordsMatch && <div className="text-red-500">Passwords Do Not Match</div>}
-                <button className='rounded bg-blue-500 hover:bg-blue-700 mt-3 p-2' onSubmit={handleFormSumbit}>Sign Up</button>
+                <button className='rounded bg-blue-500 hover:bg-blue-700 mt-3 p-2' onSubmit={handleFormSumbit} id="signUpBtn" >Sign Up</button>
             </form>
         </div>);
 }
