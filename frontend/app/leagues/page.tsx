b0e5a1ed61b818ffa916c43950e86febbf2359c8
jordanlkfung <jordanlkfung@gmail.com>
2024-12-03 20:59:01 -0800

    create model

--- frontend/app/leagues/page.tsx
+++ frontend/app/leagues/page.tsx
@@ -19,6 +19,121 @@ interface Season {
   games: string[]
 }
 
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+  const [sport, setSport] = useState<string>("Select Sport");
+  const [name, setName] = useState("");
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const router = useRouter();
+
+  const handleLeagueNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
+  const handleCreateLeague = async () => {
+    if (sport === "Select sport") {
+      setErrorMsg("Please Select a sport");
+      return
+    }
+
+    if (name.length < 3) {
+      setErrorMsg("Name has to be at least 3 characters")
+      return
+    }
+
+    const response = await fetch('api/leagues', { method: 'POST' },);
+    if (!response.ok) {
+      setErrorMsg("Error occurred while create league, please try again");
+      return
+    }
+
+    const league = response.json();
+
+    //GET id and push to [leagueId]
+    router.push(`leagues/${league}`);
+  }
+  return (
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-xl w-1/3 flex items-center justify-center">
+        <form className="grid grid-rows-1 gap-2">
+          <label htmlFor="name" className="mb-0 text-blue-400">Name</label>
+          <input
+            type="text"
+            className="rounded-md text-black p-3 w-80 border-blue-500 outline-1 border-2"
+            name="name"
+            id="name"
+            value={name}
+            onChange={handleLeagueNameChange}
+            required
+          />
+          <label htmlFor="sport" className="mb-0 text-blue-400">Sport</label>
+          {/* <select name="sport" className="border-2 border-blue-500 rounded-md p-3 w-80 text-black">
+            <option value="Basketball">Basketball</option>
+            <option value="Football">Football</option>
+          </select> */}
+          <div className="relative inline-block text-center">
+            <div className="relative group">
+              <button
+                type="button"
+                className="inline-flex justify-center rounded-lg items-center w-full px-4 py-3 text-sm font-medium text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {sport}
+              </button>
+
+              <div
+                className="absolute left-0 w-full mt-1 origin-top-left bg-white divide-y divide-gray-100 rounded-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-out"
+                role="menu"
+                aria-labelledby="sport-button"
+              >
+                <div className="py-1 w-full">
+                  <a
+                    href="#"
+                    className="block px-6 py-2 text-lg text-gray-700 hover:bg-gray-200"
+                    onClick={() => setSport("Basketball")}
+                    role="menuitem"
+                  >
+                    Basketball
+                  </a>
+                  <a
+                    href="#"
+                    className="block px-6 py-3 text-lg text-gray-700 hover:bg-gray-200 w-full"
+                    onClick={() => setSport("Football")}
+                    role="menuitem"
+                  >
+                    Football
+                  </a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className="flex justify-center gap-4 mt-4">
+            <button
+              className="bg-red-700 text-white px-4 py-2 rounded-lg w-1/2"
+              onClick={onClose}>
+              Cancel
+            </button>
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-lg w-1/2"
+              onClick={handleCreateLeague}>
+              Create
+            </button>
+          </div>
+          {errorMsg && <div className="text-red-500 p-0 mb-0">{errorMsg}</div>}
+        </form>
+
+      </div>
+    </div>
+  );
+
+
+}
 const LeaugeView = (leagues: League[]) => {
   const router = useRouter();
 
@@ -63,7 +178,10 @@ export default function League() {
   const [leagues, setAllLeagues] = useState([]);
   const [userLeagues, setUserLeagues] = useState([]);
   const [view, setView] = useState("All Leagues");
+  const [modelOpen, setModelOpen] = useState(false);
 
+  const openModal = () => setModelOpen(true);
+  const closeModal = () => setModelOpen(false);
   useEffect(() => {
     const fetchAllLeagues = async () => {
       try {
@@ -94,7 +212,7 @@ export default function League() {
 
         if (userID !== null && userID !== "") {
           const response = await fetch(`/api/users/leagues?id=${userID}`, {
-            method: "GET", 
+            method: "GET",
             headers: {
               "Content-Type": "application/json",
             },
@@ -107,7 +225,7 @@ export default function League() {
           const data = await response.json();
           console.log(data)
           setUserLeagues(data);
-        } 
+        }
       }
       catch (e) {
         console.error("An error occurred while fetching user leagues:", e);
@@ -130,9 +248,11 @@ export default function League() {
     </div>
     {view === "All Leagues" ? LeaugeView(leagues) : LeaugeView(userLeagues)}
     <div className="absolute bottom-4 right-4">
-      <button className="px-6 py-3 bg-green-600 hover:bg-green-500 text-white font-semibold rounded-lg shadow-md transition">
+      <button className="px-6 py-3 bg-green-600 hover:bg-green-500 text-white font-semibold rounded-lg shadow-md transition" onClick={openModal}>
         Create New League
       </button>
+      <Modal isOpen={modelOpen} onClose={closeModal} />
+
     </div>
   </div>
 }
